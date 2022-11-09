36aea5d9aed839887b6649652d2ce1b60992f313
MD SAIFUR RAHMAN <saifurnstuiit223344@gmail.com>
2022-11-10 00:32:28 +0600

    jwt generate successfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
