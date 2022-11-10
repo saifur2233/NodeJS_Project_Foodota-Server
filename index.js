66b974bd943154ffa79f012aa4d8ee32da45ef78
MD SAIFUR RAHMAN <saifurnstuiit223344@gmail.com>
2022-11-10 23:38:52 +0600

    final changes added

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://saifur22:gPq02O7O47Ijyzgy@cluster0.g79cke1.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.g79cke1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
